e4aaacd541976ff667df7308c0a6e33b2af03a1b
achmad-irfan <achmad.irfan1@yahoo.com>
2024-06-21 08:29:57 +0700

    skillllll

--- skill/models.py
+++ skill/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class listskill(models.Model):
     name_skill = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='/img')
+    image = models.ImageField(upload_to='img')
 
     def _str_(self):
         return "{}".format(self.name_skill)
