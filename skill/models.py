12b11e608558580b0046a18734ebbfee3bcb2a47
achmad-irfan <achmad.irfan1@yahoo.com>
2024-06-22 09:35:03 +0700

    img skil 2

--- skill/models.py
+++ skill/models.py
@@ -7,5 +7,5 @@ class listskill(models.Model):
     name_skill = models.CharField(max_length=50)
     image = models.ImageField(upload_to='img')
 
-    def _str_(self):
-        return "{}".format(self.name_skill)
+    def __str__(self):
+        return self.name_skill
