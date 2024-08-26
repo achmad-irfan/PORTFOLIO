a3c19fa73e9a79d7e61285a7af87636e8f7b6d2b
Achmad Irfan <achmad.irfan1@yahoo.com>
2024-08-26 13:10:53 +0700

    project baru 2

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,16 @@
+from .models import Proyek
+from django.views.generic import ListView
 from typing import Any, Dict
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-# Create your views here.
-from .models import Proyek
 
 
 class detailProjectView(DetailView):
     model = Proyek
+    template_name = 'projects/reports.html'
     extra_context = {
         'title': 'Detail Projects',
+        'classes': ['purpose', 'datasets', 'praprocess', 'arsitektur', 'training', 'evaluation', 'conclution', 'improvment']
     }
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
@@ -17,12 +19,12 @@ class detailProjectView(DetailView):
 
 # class indexView(ListView):
 #     model = Proyek
-#     ordering = ['tanggal']
+#     ordering = ['-tanggal']
 #     paginate_by = 5
 #     extra_context = {
 
 #         'title': 'Projects',
-#         'item': ['SQL', 'Power BI', 'Tableau', 'Dash', 'Django']}
+#         'item': ['SQL', 'Power BI', 'Tableau', 'Dash', 'Django', 'Data Science', 'Classification']}
 
 #     def get_queryset(self):
 #         category = self.kwargs.get('category')
@@ -34,6 +36,30 @@ class detailProjectView(DetailView):
 #         self.kwargs.update(self.extra_context)
 #         kwargs = self.kwargs
 #         return super().get_context_data(*args, **kwargs)
+
+
+class indexView(ListView):
+    model = Proyek
+    ordering = ['-tanggal']
+    paginate_by = 5
+    template_name = 'projects/index.html'
+
+    # Menambahkan extra_context langsung di dalam get_context_data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Projects'
+        context['item'] = ['SQL', 'Power BI', 'Tableau',
+                           'Dash', 'Django', 'Data Science', 'Classification']
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()  # Mendapatkan queryset dasar dari ListView
+        category = self.kwargs.get('category')
+
+        if category and category != "ALL":
+            queryset = queryset.filter(category=category)
+
+        return queryset
 
 
 # def index(request):
@@ -56,27 +82,6 @@ class detailProjectView(DetailView):
 
 #     return render(request, 'projects/detailProjects.html', context)
 
-class indexView(ListView):
-    model = Proyek
-    ordering = ['-tanggal']
-    paginate_by = 5
-
-    # Menambahkan extra_context langsung di dalam get_context_data
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Projects'
-        context['item'] = ['SQL', 'Power BI', 'Tableau',
-                           'Dash', 'Django', 'Data Science', 'Classification']
-        return context
-
-    def get_queryset(self):
-        queryset = super().get_queryset()  # Mendapatkan queryset dasar dari ListView
-        category = self.kwargs.get('category')
-
-        if category and category != "ALL":
-            queryset = queryset.filter(category=category)
-
-        return queryset
 
 def report_project(request, proyek_slug):
     proyek = get_object_or_404(Proyek, slug=proyek_slug)
